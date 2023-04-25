1cb363bb591b1f292a1db95bad32dc606a9e3904
Nearh777 <nearh777@gmail.com>
2023-04-25 23:36:23 +0300

    start task 01

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App  from './App'
 import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+// ReactDOM.render(
+//   <App />, document.querySelector('#root'));
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from './components/App';
+
+// import user from './user.json';
+// import './index.css';
+
+
+
+// const data = user{};
+
+
+
+// {/* <Profile 
+// avatarUrl = {user.avatar}
+// userName = {user.username}
+// tagUser = {user.tag}
+// location = {user.location}
+// followers = {user.stats.followers}
+// views = {user.stats.views}
+// likes = {user.stats.likes}
+
+// /> */}
+
+
+
+
